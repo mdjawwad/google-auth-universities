e4328b42a34a3b53957aaac98fa812765c87d40f
mdjawwad <mdjawwad790@gmail.com>
2024-12-16 13:08:42 +0530

    enviroments setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    googleClientId: '758590368190-sp25ub1u8uqekp6e0va58obgr94vcic9.apps.googleusercontent.com>',
+    googleClientId: '758590368190-sp25ub1u8uqekp6e0va58obgr94vcic9.apps.googleusercontent.com',
     redirectUri: 'http://localhost:4200',
   };
